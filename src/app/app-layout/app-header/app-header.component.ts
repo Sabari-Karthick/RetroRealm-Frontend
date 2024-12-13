b8ebf8437f7b4e99eb2edaf0f7de799bac4260e6
SK <sabariks2003@gmail.com>
2024-12-13 17:11:53 +0530

    Header Finished Partially

--- src/app/app-layout/app-header/app-header.component.ts
+++ src/app/app-layout/app-header/app-header.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
+import { Button } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ToolbarModule, AvatarModule],
+  imports: [ToolbarModule, AvatarModule, Button, MenuModule],
   templateUrl: './app-header.component.html',
-  styleUrl: './app-header.component.scss'
+  styleUrl: './app-header.component.scss',
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit {
+  items!: MenuItem[];
+  retroMenuItems!: MenuItem[];
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh',
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload',
+          },
+        ],
+      },
+    ];
+
+    this.retroMenuItems =[
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh',
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload',
+          },
+        ],
+      },
+    ];
+  }
+
+  test(): void {
+    alert('Hello World');
+  }
+
+  call(): void {
+    alert('Call ..');
+  }
+
+  notify(): void {
+    alert('Get my notifications ..');
+  }
 }
