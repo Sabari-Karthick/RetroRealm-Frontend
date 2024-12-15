a423905215b730f70edd1e9a4949ad905efa0cd4
SK <sabariks2003@gmail.com>
2024-12-15 23:40:55 +0530

    Child Route Added for App Layout

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AppHeaderComponent } from "./app-header/app-header.component";
 import { AppFooterComponent } from "./app-footer/app-footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [AppHeaderComponent, AppFooterComponent],
+  imports: [AppHeaderComponent, AppFooterComponent,RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss'
 })
