bd6171b1485a401a5a26e723872ce16aeb862402
SK <sabariks2003@gmail.com>
2025-01-05 22:06:32 +0530

    Add cart details component and routing to the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { AppHomeComponent } from './app-home/app-home.component';
 import { GameListComponent } from './game/game-list/game-list.component';
 import { GameViewComponent } from './game/game-view/game-view.component';
+import { CartDetailsComponent } from './cart/cart-details/cart-details.component';
 
 export const routes: Routes = [
   {
@@ -18,17 +19,23 @@ export const routes: Routes = [
       {
         path: 'home',
         component: AppHomeComponent,
-        title:'Retro Home'
+        title: 'Retro Home',
       },
       {
-        path: 'games',
+        path: 'games', //Need to group games under a single parent route
         component: GameListComponent,
-        title:'Retro Games'
-      },{
+        title: 'Retro Games',
+      },
+      {
         path: 'game/:id',
         component: GameViewComponent,
-        title:'Game Details'
-      }
+        title: 'Game Details',
+      },
+      {
+        path: 'cart',
+        component: CartDetailsComponent,
+        title: 'User Cart',
+      },
     ],
   },
 ];
